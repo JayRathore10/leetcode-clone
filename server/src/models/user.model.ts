25dc6f41ed4b51778bf33120471460946ce65790
Jay <jayrathore88155@gmail.com>
2026-01-13 19:59:44 +0530

    Created User Model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+export interface UserInterface {
+  username : string , 
+  email : string , 
+  password : string  , 
+  role : "user" | "admin" ,
+  createdAt : Date 
+}
+
+const userSchmea = new mongoose.Schema<UserInterface>({
+  username : {
+    type : String , 
+    required : [true , "username is required"] , 
+    minLength : 3 , 
+    maxLength : 20
+  } , 
+  email : {
+    type : String , 
+    required : [true, "Email is required"] , 
+    trim : true , 
+    unique : true  ,
+    lowercase : true , 
+    match : [/\S+@\S+\.\S+/, 'Please fill a valid email address']
+  } , 
+  password : {
+    type : String , 
+    required : [true , "Password is required"] ,
+    minLength : 6
+  } ,
+  role : {
+    type : String  ,
+    enum : ["user" , "admin"], 
+    default : "user"
+  } ,
+  createdAt : {
+    type : Date , 
+    default : Date.now
+  }
+} , {timestamps : true});
+
+export const userModel = mongoose.model<UserInterface>("user" , userSchmea);
+

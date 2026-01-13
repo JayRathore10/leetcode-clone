6a888f974147cf10a59ee282b58a196e8b2e76fc
Jay <jayrathore88155@gmail.com>
2026-01-13 21:18:05 +0530

    Create ErrorMiddleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
 import express  , {Request , Response} from "express";
+import { errorMiddleware } from "./middleware/error.middleware";
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 app.get("/"  , (req : Request, res : Response)=>{
   res.send("Hi, Jexts here!")
 })
+
+app.use(errorMiddleware);
 
 export default app;

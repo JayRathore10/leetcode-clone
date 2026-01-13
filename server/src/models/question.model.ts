67466eeee28a4986b268670f524a4e0fd0a4a4c3
Jay <jayrathore88155@gmail.com>
2026-01-13 20:16:39 +0530

    Create Question Model

---
+++ server/src/models/question.model.ts
@@ -0,0 +1,39 @@
+import mongoose, { mongo } from "mongoose";
+
+export interface QuestionInterface {
+  title : string, 
+  description : string , 
+  difficulty  : "Easy" | "Medium" | "Hard" , 
+  tags : string[] , 
+  constraints : string[] , 
+  example : string 
+}
+
+const questionSchema = new mongoose.Schema<QuestionInterface>({
+  title : {
+    type : String ,
+    required : [true , "Question title is needed"] , 
+  },
+  description : {
+    type : String  , 
+    required : [true, "Question description is needed"] 
+  }, 
+  difficulty : {
+    type : String ,
+    enum : ["Easy" , "Medium" , "Hard"]
+  } , 
+  tags : {
+    type : Array(String),
+    default : []
+  } ,
+  constraints : {
+    type : Array(String)
+  } , 
+  example : {
+    input : String  , 
+    output : String , 
+    explanation : String
+  }
+}, {timestamps : true});
+
+export const questionModel = mongoose.model<QuestionInterface>("question", questionSchema)

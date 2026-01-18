e8bcf6046c074207bf75a8cb4b9719d313dffe12
Jay <jayrathore88155@gmail.com>
2026-01-18 14:44:19 +0530

    Create IsUserLoggedIn

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,9 +1,29 @@
 import {Request , Response , NextFunction} from "express";
 import jwt from "jsonwebtoken";
+import { authRequest, userPlayLoad } from "../types/authRequest.type";
+import { JWT_SECRET } from "../configs/env.config";
+import { userModel } from "../models/user.model";
 
-export const isUserLoggedIn = async(req : Request , res : Response , next : NextFunction)=>{
+export const isUserLoggedIn = async(req : authRequest , res : Response , next : NextFunction)=>{
   try{
-    
+    const token = req.cookies.token ;
+
+    if(!token){
+      return res.status(401).json({
+        success : false , 
+        message : "Token not Found"
+      })
+    }
+
+    const decodeData = jwt.verify(token , JWT_SECRET as string) as userPlayLoad;
+
+    const user  = await userModel.findOne({
+      email : decodeData.email
+    }).select("-password");
+
+    req.user = user;
+    next();
+
   }catch(error){
     next(error);
   }
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import { loginUser, logoutUser, registerNewUser } from "../controllers/auth.controller";
+import { isUserLoggedIn } from "../middleware/auth.middleware";
 
 export const authRouter = Router();
 
 authRouter.post("/register" , registerNewUser);
 authRouter.post("/login" , loginUser);
 // protected route
-authRouter.get("/logout" , logoutUser);
+authRouter.get("/logout", isUserLoggedIn , logoutUser);

a445e33527ea52a6ebec7ad458a8caab8191e1c6
Jay <jayrathore88155@gmail.com>
2026-01-19 10:46:50 +0530

    Add Some Task

--- server/src/routes/question.routes.ts
+++ server/src/routes/question.routes.ts
@@ -3,8 +3,18 @@ import { addQuestion, deleteQuestion, run, submitCode } from "../controllers/que
 
 export const questionRouter = Router();
 
-// All Needed to be Admin protected 
+// Needed to be Admin protected 
 questionRouter.delete("/delete/:questionId" , deleteQuestion);
 questionRouter.post("/add" , addQuestion); 
+
+// needed to be user Protected 
 questionRouter.post("/run", run);
 questionRouter.post("/submit" , submitCode );
+
+
+/**
+ * Task : 
+ * Make the sumit to connect with the database and submission model
+ * via frontend we call the sumbit and then we save the data to the submission model 
+ * 
+ */

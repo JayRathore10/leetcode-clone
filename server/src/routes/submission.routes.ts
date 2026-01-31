50c933cef7e6415fa2e2387359746b70b7a8d06c
Jay <jayrathore88155@gmail.com>
2026-01-31 15:16:32 +0530

    Make addSubmission route protected

--- server/src/routes/submission.routes.ts
+++ server/src/routes/submission.routes.ts
@@ -1,7 +1,8 @@
 import {Router} from "express";
 import { addSubmission } from "../controllers/submission.controller";
+import { isUserLoggedIn } from "../middleware/auth.middleware";
 
 export const submissionRouter = Router();
 
 // User protected route 
-submissionRouter.post("/" , addSubmission);
+submissionRouter.post("/" , isUserLoggedIn , addSubmission);

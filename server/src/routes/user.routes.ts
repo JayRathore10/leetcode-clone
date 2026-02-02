3f208c2c6d8bcd8acaf59769c02445d9c9524cbc
Jay <jayrathore88155@gmail.com>
2026-02-02 20:04:47 +0530

    Make some important routes only proctected

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getAllSubmission, getAllUsers, getByUsername, getUserProfile, test } from "../controllers/user.controller";
+import { isUserLoggedIn } from "../middleware/auth.middleware";
 
 export const userRouter = Router();
 
@@ -7,6 +8,7 @@ userRouter.get("/test" , test);
 // Make this route admin procted  
 userRouter.get("/all", getAllUsers);
 // User protected route 
-userRouter.get("/profile"  , getUserProfile);
-userRouter.get("/:username" , getByUsername);
+userRouter.get("/profile" , isUserLoggedIn , getUserProfile);
+
+userRouter.get("/:username"  , getByUsername);
 userRouter.get("/:username/all-submissions" , getAllSubmission);

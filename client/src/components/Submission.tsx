e81371f466c4505d3c6cc7ecd1b4b337156cd0ae
Jay <jayrathore88155@gmail.com>
2026-02-03 22:03:24 +0530

    Add header to Submission page

--- client/src/components/Submission.tsx
+++ client/src/components/Submission.tsx
@@ -4,8 +4,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { env } from "../configs/env.config";
 import { Submission as SubmissionType } from "./Profile";
+import { LoginProps } from "./Login";
+import { Header } from "./Header";
 
-export function Submission() {
+export function Submission({ isloggedIn }: LoginProps) {
   const { id } = useParams<{ id: string }>();
   const [submission, setSubmission] = useState<SubmissionType | null>(null);
 
@@ -22,32 +24,37 @@ export function Submission() {
   if (!submission) return null;
 
   return (
-    <div className="sd-container">
-      <div className="sd-header">
-        <h1 className="sd-title">{submission.title}</h1>
+    <>
 
-        <div className="sd-badges">
-          <span
-            className={`sd-badge difficulty ${submission.questionId.difficulty.toLowerCase()}`}
-          >
-            {submission.questionId.difficulty}
-          </span>
+      <Header
+        isloggedIn={isloggedIn}
+      />
+      <div className="sd-container">
+        <div className="sd-header">
+          <h1 className="sd-title">{submission.title}</h1>
 
-          <span
-            className={`sd-badge status ${
-              submission.status === "Accepted" ? "accepted" : "wrong"
-            }`}
-          >
-            {submission.status}
-          </span>
+          <div className="sd-badges">
+            <span
+              className={`sd-badge difficulty ${submission.questionId.difficulty.toLowerCase()}`}
+            >
+              {submission.questionId.difficulty}
+            </span>
+
+            <span
+              className={`sd-badge status ${submission.status === "Accepted" ? "accepted" : "wrong"
+                }`}
+            >
+              {submission.status}
+            </span>
+          </div>
+        </div>
+
+        <div className="sd-code-container">
+          <pre className="sd-code">
+            <code>{submission.code}</code>
+          </pre>
         </div>
       </div>
-
-      <div className="sd-code-container">
-        <pre className="sd-code">
-          <code>{submission.code}</code>
-        </pre>
-      </div>
-    </div>
+    </>
   );
 }

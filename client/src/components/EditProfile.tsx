ba34413e6c97e1595abc524379dbb26ff6c42be7
Jay <jayrathore88155@gmail.com>
2026-02-06 15:46:59 +0530

    Create and design the logic of edit profile component

--- client/src/components/EditProfile.tsx
+++ client/src/components/EditProfile.tsx
@@ -1,12 +1,83 @@
 import { LoginProps } from './Login';
 import "../styles/EditProfile.css";
 import { Header } from './Header';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { env } from '../configs/env.config';
 export function EditProfile({isloggedIn}: LoginProps){
+
+  const [preview  , setPreview] = useState<string>("");
+  const [name , setName] = useState<string>("");
+  const [profilePic , setProfilePic] = useState<File | null>(null);
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{  
+    const fetchUser = async()=>{
+      try{
+        const response = await axios.get(`${env.backendUrl}/api/users/profile`);
+
+        setName(response.data.user.name);
+        setPreview(`${env.backendUrl}/images/${response.data.user.profilePic}`);
+
+      }catch(error){
+        console.log(error);
+      }
+    }
+    fetchUser();
+  } , []);
+
+  const handleSubmit = async(e : React.FormEvent)=>{
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("name" , name);
+    if(profilePic) formData.append("profilePic" , profilePic);
+
+    await axios.put(
+      `${env.backendUrl}/api/user/profile` , 
+      formData ,
+      {headers : {"Content-Type" : "multipart/form-data"}}
+    );
+    navigate("/profile");
+  }
+
+  const handleImageChange = (e : React.ChangeEvent<HTMLInputElement>)=>{
+    if(!e.target.files) return ;
+    const file = e.target.files[0];
+    setProfilePic(file);
+    setPreview(URL.createObjectURL(file));
+  }
+
   return(
     <>
       <Header 
         isloggedIn={isloggedIn}
       />
+         <div className="ep-edit-profile-page">
+      <h2>Edit Profile</h2>
+
+      <form onSubmit={handleSubmit} className="ep-edit-profile-form">
+        <div className="ep-image-section">
+          <img src={preview} className="ep-avatar large" />
+          <input type="file" accept="image/*" onChange={handleImageChange} />
+        </div>
+
+        <div className="ep-input-group">
+          <label>Name</label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+
+        <button
+        className='ep-save-btn'
+        type="submit">Save Changes</button>
+      </form>
+    </div>
     </>
   );
 }

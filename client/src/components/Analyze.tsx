a16589d701f4effe68a397a6623e5cae9c107e7b
Jay <jayrathore88155@gmail.com>
2026-02-09 20:09:56 +0530

    Fetching submission detail to the analyze page

--- client/src/components/Analyze.tsx
+++ client/src/components/Analyze.tsx
@@ -1,8 +1,30 @@
+import { useParams } from "react-router-dom";
 import "../styles/Analyze.css";
 import { Header } from "./Header";
 import { LoginProps } from "./Login";
+import { useEffect, useState } from "react";
+import { Submission as SubmissionType } from "./Profile";
+import axios from "axios";
+import { env } from "../configs/env.config";
 
 export function Analyze({isloggedIn} : LoginProps){
+  const {id} = useParams<{id : string}>();
+
+  const [analyze , setAnalyze] = useState<string>("");
+  const [submission , setSubmission] = useState<SubmissionType | null>(null);
+
+  useEffect(()=>{
+    const fetchSubmissionDetail = async()=>{
+      try{
+        const response = await axios.get(`${env.backendUrl}/api/submission/${id}`);
+        setSubmission(response.data.submission);
+      }catch(error){
+        console.log(error);
+      }
+    }
+    fetchSubmissionDetail();
+  } , [id]);
+
   return(
     <>
       <Header isloggedIn={isloggedIn} />

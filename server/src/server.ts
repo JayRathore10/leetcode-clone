a468098e7a9ba4eb881043a34b20a09e381509ca
Jay <jayrathore88155@gmail.com>
2026-02-15 17:26:13 +0530

    Minor fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,7 @@ import { connectDatabase } from "./database/mongoDB";
 
 const PORT = process.env.PORT || 3000;
 
-
 app.listen(PORT, async() => {
-  console.log(`Server running on http://localhost:${PORT}`);
   await connectDatabase();
+  console.log(`Server running on http://localhost:${PORT}`);
 });

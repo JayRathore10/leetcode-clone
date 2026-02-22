89182e6f518ccb7e1931d384f7354e754decb494
Jay <jayrathore88155@gmail.com>
2026-02-22 15:36:17 +0530

    Add token to login response

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -109,7 +109,8 @@ export const loginUser = async (req: Request, res: Response, next: NextFunction)
     return res.status(200).json({
       success: true,
       message: "Login successfully",
-      user
+      user , 
+      token
     })
 
   } catch (err) {

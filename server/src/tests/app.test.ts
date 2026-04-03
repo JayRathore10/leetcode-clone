edd2a83bc765714c6a55752b47f0f274494961ad
Jay <jayrathore88155@gmail.com>
2026-04-03 22:15:04 +0530

    Add / test

--- server/src/tests/app.test.ts
+++ server/src/tests/app.test.ts
@@ -1,6 +1,15 @@
 import request from "supertest";
 import app from "../app";
 
+describe("GET /" , ()=>{
+  it("should return Jexts message" , async()=>{
+    const res = await  request(app).get("/");
+    expect(res.status).toBe(200);
+
+    expect(res.text).toBe("Hi, Jexts here!");
+  })
+})
+
 describe("GET test/" , ()=>{
   it("should return Test message" , async()=>{
     const res = await request(app).get("/test");

211db0cd26ab4a53e4e21dcff8257969cee5d74c
Jay <jayrathore88155@gmail.com>
2026-04-04 19:40:26 +0530

    Add user not found in database test

--- server/src/tests/auth.test.ts
+++ server/src/tests/auth.test.ts
@@ -11,10 +11,10 @@ jest.mock("jsonwebtoken");
 describe("POST /api/auth/login" , ()=>{
   it("should login successfully", async()=>{
     // mocking the DB using the jest mock 
-    (userModel.findOne as jest.Mock).mockReturnValue({
+    (userModel.findOne as jest.Mock).mockResolvedValue({
       email : "test@gmail.com"  , 
       password : "hashedPassword", 
-    });
+    }); 
 
     // mocking bcrypt.compare(passowrd , user.password) 
     (bcrypt.compare as jest.Mock).mockReturnValue(true);
@@ -38,4 +38,20 @@ describe("POST /api/auth/login" , ()=>{
     expect(cookies).toBeDefined();
     expect(cookies[0]).toContain("token=fake_token");
   });
+
+  it("does not found user in Database" , async()=>{
+    // this null means user not found in our database 
+    (userModel.findOne as jest.Mock).mockResolvedValue(null);
+
+    const res = await request(app).
+      post("/api/auth/login").
+      send({
+        email : "test@gmail.com" , 
+        password : "12345"
+      });
+
+    expect(res.status).toBe(404);
+    expect(res.body.success).toBe(false);
+  })
+
 });

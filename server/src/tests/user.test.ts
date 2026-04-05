196573a78b3c799929ca1807d6321247f256a890
Jay <jayrathore88155@gmail.com>
2026-04-05 22:44:55 +0530

    Add test for user /all route

--- server/src/tests/user.test.ts
+++ server/src/tests/user.test.ts
@@ -4,7 +4,7 @@ import request from "supertest";
 
 jest.mock("../models/user.model");
 
-describe("GET /test"  , ()=>{
+describe("GET /api/users/test"  , ()=>{
   it("should return 200 for success run" , async()=>{ 
     const res = await request(app).
       get("/api/users/test");
@@ -14,3 +14,15 @@ describe("GET /test"  , ()=>{
     expect(res.body.message).toBe("Hello");
   });
 });
+
+describe("GET /api/users/all" , ()=>{
+  it("should return 404 when there is no user in database"  , async()=>{
+    (userModel.find as jest.Mock).mockResolvedValue ([]);
+
+    const res = await request(app).
+      get("/api/users/all");
+
+    expect(res.status).toBe(404);
+
+  })
+})

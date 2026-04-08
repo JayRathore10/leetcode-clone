b6d27256435459df0811e6683689f1b8d3eaab39
Jay <jayrathore88155@gmail.com>
2026-04-08 22:04:51 +0530

    Add test for successfully question deletion

--- server/src/tests/question.test.ts
+++ server/src/tests/question.test.ts
@@ -9,7 +9,7 @@ jest.mock("../middleware/auth.middleware", () => ({
 }));
 
 describe("DELETE /api/question/delete/:questionId", () => {
-  it("should return 404 when question not found", async () => {
+  it("should return 404 when question not found in database for deletion", async () => {
     (questionModel.findByIdAndDelete as jest.Mock).mockResolvedValue(null);
 
     const res = await request(app).
@@ -20,6 +20,21 @@ describe("DELETE /api/question/delete/:questionId", () => {
       success: false,
       message: "Question Not found"
     });
+  });
 
-  })
+  it("should return 200 when it successfully delete question", async () => {
+    (questionModel.findByIdAndDelete as jest.Mock).mockResolvedValue({
+      _id: "1234"
+    });
+
+    const res = await request(app).
+      delete("/api/question/delete/1234");
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      success: true,
+      message: "Question delete successfully"
+    });
+
+  });
 })

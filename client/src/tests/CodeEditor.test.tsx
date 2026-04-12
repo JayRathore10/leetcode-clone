bc6168a38d749b204f6ca71bfffc013464eb9fbb
Jay <jayrathore88155@gmail.com>
2026-04-12 20:54:43 +0530

    Add test fallback to cpp when no language

--- client/src/tests/CodeEditor.test.tsx
+++ client/src/tests/CodeEditor.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen} from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CodeEditor } from "../components/CodeEditor";
 
 jest.mock("@monaco-editor/react", () => ({
@@ -12,52 +12,87 @@ jest.mock("@monaco-editor/react", () => ({
   ),
 }));
 
-describe("CodeEditor" , ()=>{
+describe("CodeEditor", () => {
   let code = "";
   const setCode = jest.fn((val) => {
     code = val;
   });
 
-  beforeEach(()=>{
+  beforeEach(() => {
     jest.clearAllMocks();
   })
 
-  test("renders editor" ,()=>{
+  test("renders editor", () => {
     render(
-      <CodeEditor code = {code} setCode={setCode} language="cpp" />
+      <CodeEditor code={code} setCode={setCode} language="cpp" />
     );
     expect(screen.getByTestId("editor")).toBeInTheDocument();
   });
 
-  test("sets boilerplate when language changes", ()=>{
+  test("sets boilerplate when language changes", () => {
     render(
-      <CodeEditor 
-        code={code} 
-        setCode={setCode} 
+      <CodeEditor
+        code={code}
+        setCode={setCode}
         language="python"
       />
     );
     expect(setCode).toHaveBeenCalled();
-    const calledWith = setCode.mock.calls[0][0] ;
+    const calledWith = setCode.mock.calls[0][0];
     expect(calledWith).toContain("def solve()");
   });
 
-  test("updates code on change" , ()=>{
+  test("updates code on change", () => {
     render(
-      <CodeEditor 
-        code={code} 
-        setCode={setCode} 
+      <CodeEditor
+        code={code}
+        setCode={setCode}
         language="javascript"
       />
     );
 
     const editor = screen.getByTestId("editor");
 
-    fireEvent.change(editor , {
-      target : {value : "console.log('hello)"}
+    fireEvent.change(editor, {
+      target: { value: "console.log('hello)" }
     });
 
     expect(setCode).toHaveBeenCalledWith("console.log('hello)");
 
+  });
+
+  test("fallback to cpp when no language", () => {
+    render(
+      <CodeEditor
+        code={code}
+        setCode={setCode}
+        language=""
+      />
+    )
+
+    const calledWith = setCode.mock.calls[0][0];
+
+    const normalize = (str: string) =>
+      str.replace(/\s+/g, " ").trim();
+
+    expect(normalize(calledWith)).toContain(
+      normalize(`#include <bits/stdc++.h>
+        using namespace std;
+        /*
+         Write your solution here
+         IMPORTANT:
+         - Do NOT return the answer
+         - PRINT the answer using cout
+        */
+        void solve() {
+            // Example:
+            // cout << answer << endl;
+        }
+        int main() {
+            solve();
+            return 0;
+        }`)
+      );
   })
+
 })

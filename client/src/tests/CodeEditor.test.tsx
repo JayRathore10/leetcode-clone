fbfc02b5deb4793585abfa6ccff2fc4636914ea1
Jay <jayrathore88155@gmail.com>
2026-04-12 19:35:07 +0530

    Add test for codeEditor render

---
+++ client/src/tests/CodeEditor.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen} from "@testing-library/react";
+import { CodeEditor } from "../components/CodeEditor";
+
+jest.mock("@monaco-editor/react", () => ({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Editor: ({ value, onChange }: any) => (
+    <textarea
+      data-testid="editor"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  ),
+}));
+
+describe("CodeEditor" , ()=>{
+  let code = "";
+  const setCode = jest.fn((val) => {
+    code = val;
+  });
+
+  beforeEach(()=>{
+    jest.clearAllMocks();
+  })
+
+  test("renders editor" ,()=>{
+    render(
+      <CodeEditor code = {code} setCode={setCode} language="cpp" />
+    );
+    expect(screen.getByTestId("editor")).toBeInTheDocument();
+  });
+
+})

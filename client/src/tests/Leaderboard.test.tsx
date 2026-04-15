a8389572fde3e7cd3cc90829ed276e105fe56d29
Jay <jayrathore88155@gmail.com>
2026-04-15 22:23:04 +0530

    Add test for render leaderboard

---
+++ client/src/tests/Leaderboard.test.tsx
@@ -0,0 +1,24 @@
+import { screen , render } from "@testing-library/react";
+import { Leaderboard } from "../components/Leaderboard";
+import { MemoryRouter } from "react-router-dom";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate
+}));
+
+
+describe("Leaderboard Component" , ()=>{
+  test("render leaderboard" , ()=>{
+    render(
+      <MemoryRouter>
+        <Leaderboard />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Coming Soon")).toBeInTheDocument();
+
+  })
+})

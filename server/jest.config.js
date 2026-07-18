aeb3ba97c2d6bd74e3ab8e2ee52e24e2ca423b89
Jay <jayrathore88155@gmail.com>
2026-07-18 15:35:59 +0530

    fix : the submission test and ivalidation

--- server/jest.config.js
+++ server/jest.config.js
@@ -4,8 +4,13 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  roots: ["<rootDir>/src"],
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
   },
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/",
+  ],
 };
--- server/src/validation/submission.validation.ts
+++ server/src/validation/submission.validation.ts
@@ -1,9 +1,9 @@
-import {z} from "zod";
+import { z } from "zod";
 
 export const submissionSchema = z.object({
-  questionId : z.string() ,
-  code : z.string() , 
-  language : z.string() ,
-  status : z.string() ,
-  title : z.string()
-})
+  questionId: z.string(),
+  code: z.string(),
+  language: z.string(),
+  status: z.enum(["Accepted", "WA", "TLE", "MLE"]),
+  title: z.string(),
+});
